aa559953db1d0e2253656abce021d04b0300fd5b
Baklysha9 <bogdandynay@gmail.com>
2019-03-19 13:07:19 +0300

    main page start

---
+++ diploma/resources/views/layout.blade.php
@@ -0,0 +1,44 @@
+
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Пример на bootstrap 4: Прайслист - пример страницы с ценой, созданной с помощью карт, с пользовательским верхнего и нижнего колонтитула.">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Kosmetikerin Elena Brezger</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/main.css')}}">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <!-- <link href="pricing.css" rel="stylesheet"> -->
+    
+  </head>
+
+  <body>
+
+        @include('layouts.nav')
+
+        @yield('content')
+
+        @include('layouts.footer')
+  
+<!--jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="{{ asset('js/main.js')}}"></script>
+</body>
+</html>
---
+++ diploma/resources/views/layouts/nav.blade.php
@@ -0,0 +1,10 @@
+<div class="navbar d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white shadow-sm">
+  <h5 class="main_logo my-0 mr-md-auto font-weight-normal">Kosmetikerin Elena Brezger</h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="#">Features</a>
+    <a class="p-2 text-dark" href="#">Enterprise</a>
+    <a class="p-2 text-dark" href="#">Support</a>
+    <a class="p-2 text-dark" href="#">Pricing</a>
+  </nav>
+  <button type="button" class="btn btn-primary">Sign up</button>
+</div>
