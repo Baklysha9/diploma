368e0f2f64f1df8c9395fe87a6aa4347dd4c8ad3
Baklysha9 <bogdandynay@gmail.com>
2019-06-14 23:59:59 +0300

    clean code

--- diploma/resources/views/layout.blade.php
+++ diploma/resources/views/layout.blade.php
@@ -5,9 +5,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Пример на bootstrap 4: Прайслист - пример страницы с ценой, созданной с помощью карт, с пользовательским верхнего и нижнего колонтитула.">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
+    <link rel="icon" href="img/favicon.ico">
 
     <title>Kosmetikerin Elena Brezger</title>
     <!-- Bootstrap core CSS -->
@@ -23,17 +21,8 @@
  
         @include('layouts.footer')
   
-<!--jQuery-->
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="{{ asset('js/jquery.maskedinput.js') }}"></script>
-<!-- <script>
-$(".navbar a").on("click", function(){
-   $(".navbar").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
-</script> -->
-
 
 </body>
 </html>
